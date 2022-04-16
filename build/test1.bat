c08d36d21425005dc4edb7720916c5190b715c38
Jens Kallup <kallup.jens@web.de>
2022-04-16 17:18:56 +0200

    update 2

--- build/test1.bat
+++ build/test1.bat
@@ -5,7 +5,7 @@ fpc -al -Aas -CF32 -a -al -n -sh -st -Xd -Xe -Xm -WA -Us ^
     
 as --64 -o .obj/src_pp/fpc/system.o .asm/src_pp/fpc/system.s
 
-fpc -al -Aas -CF32 -a -al -n -sh -st -Ur -Xd -Xe -Xm -WA ^
+fpc -al -Aas -CF32 -a -al -n -sh -st -Ur -Xd -Xe -Xm -WA     ^
     -O2 -Mdelphi -OoPEEPHOLE -OoREMOVEEMPTYPROCS ^
     -Px86_64 -Twin64   ^
     -o.asm/test/test1.s sample/test1.pas
--- build/test2.bat
+++ build/test2.bat
@@ -1,11 +1,11 @@
-fpc -al -Aas -CF32 -a -al -n -sh -st -Xd -Xe -Xm -WA -WR -Us ^
+fpc -al -Aas -CF32 -a -al -n -sh -st -Xd -Xe -Xm -WA -Us ^
     -O2 -Mdelphi -OoPEEPHOLE -OoREMOVEEMPTYPROCS ^
     -Px86_64 -Twin64   ^
     -o.asm/src_pp/fpc/system.s src_pp/fpc/system.pas
     
 as --64 -o .obj/src_pp/fpc/system.o .asm/src_pp/fpc/system.s
 
-fpc -al -Aas -CF32 -a -al -n -sh -st -Ur -Xd -Xe -Xm -WA -WR ^
+fpc -al -Aas -CF32 -a -al -n -sh -st -Ur -Xd -Xe -Xm -WA -Us ^
     -O2 -Mdelphi -OoPEEPHOLE -OoREMOVEEMPTYPROCS ^
     -Px86_64 -Twin64   ^
     -o.asm/test/test1.s sample/test1.pas
